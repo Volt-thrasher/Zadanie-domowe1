08f969afca4631036794af4de4b9ff3597c8e523
Volt <sonianadashkevich@gmail.com>
2019-09-24 19:56:49 +0200

    Dodalam do tekstu 'i commitowania'

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -3,5 +3,7 @@ public class Zadanie {
         System.out.println("Ucze");
         System.out.println("sie");
         System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }
