45df0b05ebd695dd3dd1880a3636c37926cd336c
Volt <sonianadashkevich@gmail.com>
2019-09-24 19:54:28 +0200

    Dodalam tekst

---
+++ src/Zadanie.java
@@ -0,0 +1,7 @@
+public class Zadanie {
+    public static void main(String[] args) {
+        System.out.println("Ucze");
+        System.out.println("sie");
+        System.out.println("programowania");
+    }
+}
